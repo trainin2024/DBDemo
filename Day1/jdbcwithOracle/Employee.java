89f3aa1f51004daf9bc84002ec7c7bd020593597
trainin2024 <ashujauhari2024@gmail.com>
2024-07-27 22:10:14 +0530

    Add files via upload

---
+++ Day1/jdbcwithOracle/Employee.java
@@ -0,0 +1,52 @@
+package jdbcwithOracle;
+
+public class Employee {
+	long id;
+	String firstname;
+	String  lastname;
+	String email;
+	public Employee(long id, String firstname, String lastname, String email) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+	}
+	
+	public Employee()
+	{}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
